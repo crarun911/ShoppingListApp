f6e28598906326ea8d7e6b48ea92eb0aa83e58a4
Arun Rajendran <crarun7453@gmail.com>
2025-03-09 13:48:51 +0000

    alert dialogue added

--- app/src/main/java/com/dcoders/shoppinglistapp/ShoppingList.kt
+++ app/src/main/java/com/dcoders/shoppinglistapp/ShoppingList.kt
@@ -2,14 +2,24 @@ package com.dcoders.shoppinglistapp
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -18,21 +28,23 @@ import androidx.compose.ui.unit.dp
 data class ListItem(val id:Int,
                     var name:String,
                     var quantity:Int,
-                    var isEditable:Boolean)
+                    var isEditable:Boolean=false)
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ShoppingListApp(){
-    var shoppingList= remember {
-        mutableListOf<ListItem>()
-    }
+    var shoppingList= remember { mutableListOf<ListItem>() }
+    var showDialogue by remember { mutableStateOf(false)   }
+    var itemName by remember { mutableStateOf("")    }
+    var itemQuantity by remember { mutableStateOf("")    }
 
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center
     ) {
         Button(
-            onClick = { },
+            onClick = { showDialogue=true },
             modifier = Modifier.align(Alignment.CenterHorizontally)
         ) {
             Text(text = "Add Item")
@@ -45,5 +57,48 @@ fun ShoppingListApp(){
 
             }
         }
+    }
+    if (showDialogue){
+       AlertDialog(
+           onDismissRequest = { showDialogue=false },
+           title ={ Text(text = "Hello Sample Dialogue")},
+           text = {
+                  Column(){
+
+                      OutlinedTextField(modifier = Modifier.padding(8.dp), value = itemName, onValueChange = {
+                          itemName=it
+                      })
+                      OutlinedTextField(modifier = Modifier.padding(8.dp),value = itemQuantity, onValueChange = {
+                          itemQuantity=it
+                      })
+                  }
+           },
+           confirmButton =  {
+               Row(modifier = Modifier.fillMaxWidth().padding(8.dp),
+                   horizontalArrangement = Arrangement.SpaceEvenly
+               ) {
+                   Button(onClick = {
+
+                       if (itemName.isNotBlank()){
+                           val newItem=ListItem(id = shoppingList.size+1,
+                               name = itemName,
+                               quantity = itemQuantity.toInt()
+                               )
+
+                           shoppingList.add(newItem)
+                           showDialogue=false
+                           itemName=""
+                       }
+                   }) {
+                       Text(text = "Ok")
+                   }
+                   Button(onClick = { showDialogue=false}) {
+                       Text(text = "Cancel")
+                   }
+               }
+          }
+
+       )
+
     }
 }
